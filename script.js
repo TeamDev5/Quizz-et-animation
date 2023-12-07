258476c87b4cb545ddb75ffbf98bed597072dc3f
amel0618 <amelie.vie.av@gmail.com>
2023-12-07 16:21:18 +0100

    modifs

--- script.js
+++ script.js
@@ -23,32 +23,30 @@ const questions = [
         image: "assets/images/image3.png",
         question: "Dans ce bout de HTML, plusieurs erreurs ont été commises. Qu'est-ce qui ne va pas ?",
         choices: [
-            "La balise <p> n'est pas fermée",
             "La source de l'image n'est pas précisée",
+            "La balise <p> n'est pas fermée",
             "L'image ne possède pas d'attribut alt"
         ],
-        correctAnswer: [2, 3, 4] 
+        correctAnswer: 1
     },
     {
         question: "Une valeur et un attribut permettent, ensemble, d'ouvrir un lien dans un nouvel onglet. Lesquels ?",
         choices: [
             "L'attribut  target",
-            "La valeur  _blank",
             "L'attribut title",
-            "La valeur  mailto:",
-            "La valeur  blank"
+            "L'attribut  blank"
         ],
-        correctAnswer: [0, 1]
+        correctAnswer: 0
     },
     {
-        question: "Parmi ces affirmations à propos des commentaires, lesquelles sont correctes ?",
+        question: "Parmi ces affirmations à propos des commentaires, laquelle est correcte ?",
         choices: [
             "En Javascript, on ouvre un commentaire avec <!--",
             "En HTML, on ferme un commentaire avec //",
             "Les commentaires ne sont pas visibles dans le code source d'une page HTML",
             "Il existe des raccourcis clavier selon l'éditeur de code pour commenter du code rapidement"
         ],
-        correctAnswer: [0, 2, 3] // Plusieurs réponses sont correctes
+        correctAnswer: 3
     },
     {
         image: "assets/images/image6.png",
@@ -73,7 +71,6 @@ const questions = [
     
     {
         image: "assets/images/image8.png",
-        image: "assets/images/image8.2.png",
         question: "Quel morceau de code permet de créer un lien vers la page 1 à la place du commentaire Insérer ici un lien vers la page 1 ?",
         choices: [
             "<a href='../page1.html'>Page 1</a>",
@@ -88,24 +85,21 @@ const questions = [
         question: "Comment faire pour que la couleur du titre ainsi que du paragraphe soient #ff1493 ?",
         choices: [
             "h1, p { color: #ff1493; }",
-            "h1 { color: #ff1493; }",
-            "p { color: #ff1493; }",
+            ".h1, p {color: #ff1493;}",
             "h1 + p { color: #ff1493; }"
         ],
-        correctAnswer: [0, 1, 2] // Plusieurs réponses possibles
+        correctAnswer: 0
     },
     {
         image: "assets/images/image10.png",
-        image: "assets/images/image10.2.png",
-        question: "À partir de l'aperçu suivant, quelles propriétés de texte ont été appliquées dans les classes premier-paragraphe ou deuxieme-paragraphe ?",
+        question: "À partir de l'aperçu suivant, quelle propriété de texte a été appliquée dans les classes premier-paragraphe ou deuxieme-paragraphe ?",
         choices: [
-            "text-align",
-            "font-size",
             "text-decoration",
             "font-family",
+            "text-align",
             "color"
         ],
-        correctAnswer: [1, 4] // Plusieurs réponses possibles
+        correctAnswer: 2
     },
     {
         question: "Qu'est-ce qu'une Media Query en CSS ?",
@@ -124,15 +118,6 @@ const questions = [
             "@media"
         ],
         correctAnswer: 2
-    },
-    {
-        question: "Quelle est la signification de 'responsive design' ?",
-        choices: [
-            "Concevoir des sites web qui répondent aux commentaires des utilisateurs",
-            "Concevoir des sites web qui s'ajustent et offrent une expérience utilisateur optimale sur différents appareils et tailles d'écran",
-            "Concevoir des sites web qui chargent rapidement même avec une connexion Internet lent"
-        ],
-        correctAnswer: 1
     },
     {
         question: "Quelle est la syntaxe pour appliquer des styles uniquement aux écrans d'une largeur maximale de 768 pixels ?",
@@ -162,22 +147,13 @@ const questions = [
         correctAnswer: 2
     },
     {
-        question: "Avec quel syntaxe écrit-on un commentaire en CSS ?",
-        choices: [
-            "// Mon Commentaire",
-            "/* Mon Commentaire */",
-            "< !-- mon commentaire - - >"
-        ],
-        correctAnswer: 1
-    },
-    {
         question: "À quoi peut servir le JavaScript ?",
         choices: [
-            "Valider les données des formulaires du côté client avant leur envoi au serveur",
+            "Mettre des fichiers en base de données",
             "Optimisation des performances du réseau",
             "Réaliser des animations"
         ],
-        correctAnswer: [0, 2] // Plusieurs réponses possibles
+        correctAnswer: 2
     },
     {
         question: "Quelle est la méthode recommandée pour inclure du CSS dans une page HTML ?",
@@ -209,17 +185,6 @@ const questions = [
         ],
         correctAnswer: 0
     },
-    
-    {
-        question: "Quelle propriété CSS est utilisée pour définir la largeur d'un élément ?",
-        choices: [
-            "a) width",
-            "b) size",
-            "c) element-width",
-            "d) space-width"
-        ],
-        correctAnswer: 0
-    },
     {
         question: "Quelle propriété CSS est utilisée pour définir l'espacement intérieur autour d'un élément ?",
         choices: [
@@ -227,16 +192,6 @@ const questions = [
             "b) space",
             "c) margin",
             "d) element-spacing"
-        ],
-        correctAnswer: 0
-    },
-    {
-        question: "Quelle balise HTML est utilisée pour regrouper des éléments en tant que boîte ?",
-        choices: [
-            "a) <div>",
-            "b) <span>",
-            "c) <box>",
-            "d) <container>"
         ],
         correctAnswer: 0
     },
@@ -253,41 +208,17 @@ function loadQuestion() {
     choicesElement.innerHTML = "";
 
     if (current.image) {
+        const imagesContainer = document.createElement("div"); // Créer un conteneur pour les images
+        imagesContainer.className = "images-container"; // Ajouter une classe pour le conteneur
+    
         const imageElement = document.createElement("img");
         imageElement.src = current.image;
     
-        // Appliquer directement les styles à chaque image en fonction de la question
-        if (currentQuestion === 3) {
-            imageElement.style.width = "800px";
-        } else if (currentQuestion === 5) {
-            imageElement.style.width = "300px"; 
-        }else if (currentQuestion === 6) {
-            imageElement.style.width = "100px";
-        } else if (currentQuestion === 7) {
-            imageElement.style.width = "500px";
-        } else if (currentQuestion === 8) {
-            imageElement.style.width = "800px";
-        } else if (currentQuestion === 9) {
-            imageElement.style.width = "800px"; 
-        } else if (currentQuestion === 10) {
-            const imagesContainer = document.createElement("div"); // Créer un conteneur pour les images
-            imagesContainer.className = "images-container"; // Ajouter une classe pour le conteneur si nécessaire
-        
-            const image1 = document.createElement("img"); // Créer la première image
-            image1.src = current.image; // Utiliser la première image
-            // Définir des styles ou des classes pour la première image si nécessaire
-            imagesContainer.appendChild(image1); // Ajouter la première image au conteneur
-        
-            const image2 = document.createElement("img"); // Créer la deuxième image
-            image2.src = current.image2; // Utiliser la deuxième image
-            // Définir des styles ou des classes pour la deuxième image si nécessaire
-            imagesContainer.appendChild(image2); // Ajouter la deuxième image au conteneur
-        
-            choicesElement.appendChild(imagesContainer); // Ajouter le conteneur avec les images à l'élément des choix
-        }
+        // Ajouter une classe aux images pour gérer leur style si nécessaire
+        imageElement.className = "quiz-image";
     
-        imageElement.style.height = "auto"; // Conserver les proportions
-        choicesElement.appendChild(imageElement);
+        imagesContainer.appendChild(imageElement); // Ajouter l'image au conteneur
+        choicesElement.appendChild(imagesContainer); // Ajouter le conteneur à l'élément des choix
     }
 
 
@@ -321,15 +252,37 @@ function checkAnswer(choice) {
 }
 function showResult() {
     const quizContainer = document.querySelector(".quiz-container");
-    quizContainer.innerHTML = `<h1>Résultat :</h1><p>Score : ${score} sur ${questions.length}</p>`;
+
+    // Création des éléments HTML
+    const resultHeading = document.createElement('h1');
+    resultHeading.textContent = 'Résultat :';
+
+    const scoreParagraph = document.createElement('p');
+    scoreParagraph.textContent = `Score : ${score} sur ${questions.length}`;
+
+
+    quizContainer.appendChild(resultHeading);
+    quizContainer.appendChild(scoreParagraph);
+
     if (incorrectAnswers.length > 0) {
         const incorrectAnswersList = incorrectAnswers.map((index) => {
-            return `<li>${questions[index].question} : ${questions[index].choices[questions[index].correctAnswer]}</li>`;
+            return `${questions[index].question} : ${questions[index].choices[questions[index].correctAnswer]}`;
         });
 
-        const incorrectAnswersHTML = `<h2>Réponses incorrectes : </h2><ul>${incorrectAnswersList.join("")}</ul>`;
+        const incorrectAnswersHeading = document.createElement('h2');
+        incorrectAnswersHeading.textContent = 'Réponses incorrectes :';
 
-        quizContainer.innerHTML += incorrectAnswersHTML;
+        const incorrectAnswersUL = document.createElement('ul');
+        incorrectAnswersList.forEach(answerText => {
+            const listItem = document.createElement('li');
+            listItem.textContent = answerText;
+            incorrectAnswersUL.appendChild(listItem);
+        });
+
+        quizContainer.appendChild(incorrectAnswersHeading);
+        quizContainer.appendChild(incorrectAnswersUL);
+        
     }
 }
+
 loadQuestion();
